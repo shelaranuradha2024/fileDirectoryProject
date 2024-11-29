05175410a2c71d00ae3f6ca5e77ffa3e34a8f243
Anuradha SHelar <shelaranuradha2024@gmail.com>
2024-11-29 15:51:19 +0530

    code changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ const App = () => {
   return (
     <div className="App">
       <div className="directory-box">
-        <h1 className="file-directory-heading">File Directory</h1> 
       </div>
       <FileManager />
     </div>
