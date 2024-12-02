d33c4f6106e6a706c290074d54020bb365d96d18
shelaranuradha2024 <shelaranuradha2024@gmail.com>
2024-12-03 01:38:36 +0530

    Update File.js for Drag and Drop functionality

--- src/components/File.js
+++ src/components/File.js
@@ -1,19 +1,24 @@
 import React from "react";
+import "../styles/File.css";
 
-const File = ({ file, folderId, handleRenameFile, handleDeleteFile }) => {
+const File = ({ file, folderId, handleRenameFile, handleDeleteFile, handleDragStart }) => {
   return (
-    <div className="file">
+    <div
+      className="file"
+      draggable
+      onDragStart={(e) => handleDragStart(e, { ...file, folderId }, "file")}
+    >
       <span>{file.name}</span>
       <div className="file-actions">
         <button className="rename" onClick={() => handleRenameFile(file.id, folderId)}>
           Rename
         </button>
         <button className="delete" onClick={() => handleDeleteFile(file.id, folderId)}>
-          Delete
-        </button>
-      </div>
+        Delete
+      </button>
     </div>
-  );
+  </div>
+);
 };
 
 export default File;
